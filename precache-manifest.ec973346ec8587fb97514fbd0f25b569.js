742b047581f319d331ff46f004ddec83935438ef
xavierbriole <xavierbriole@users.noreply.github.com>
2020-06-07 12:19:46 +0000

    deploy: 7ca44929f3bfa317ec1d4efa00702c28d9c30513

---
+++ precache-manifest.ec973346ec8587fb97514fbd0f25b569.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b29ac50ad5c58f68b5fc275fb6330aac",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2fef0a65781f4868c082",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "702fa3ced4ae3059dd01",
+    "url": "./static/js/2.a5278c4a.chunk.js"
+  },
+  {
+    "revision": "d103818e803302bc4a25e2b21db0e62e",
+    "url": "./static/js/2.a5278c4a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2fef0a65781f4868c082",
+    "url": "./static/js/main.acc49b77.chunk.js"
+  },
+  {
+    "revision": "edde4c884ba352386dee",
+    "url": "./static/js/runtime-main.34b152bb.js"
+  }
+]);
